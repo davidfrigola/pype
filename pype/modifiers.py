1d930fa6e3dc258d0302ebade874ca887cb379e9
david <david.frigola@gmail.com>
2013-11-24 14:53:48 +0100

    Adding logging to modifiers

--- pype/modifiers.py
+++ pype/modifiers.py
@@ -1,6 +1,8 @@
 from pype.core import *
 from pype.extra import AbstractModifier
+import logging
 
+logger = logging.getLogger("pype.modifiers")
 
 PREPEND_VALUE = "prependvalue"
 
@@ -10,6 +12,7 @@ class PrependStringModifier(AbstractModifier):
     def __init__(self,config):
         self.config = config
         if (not PREPEND_VALUE in config) or config[PREPEND_VALUE] is None:
+            logger.error("Configuration must contain "+PREPEND_VALUE)
             raise "Configuration must contain "+PREPEND_VALUE
 
     def modify(self,item):
@@ -17,5 +20,6 @@ class PrependStringModifier(AbstractModifier):
         itemResult = BaseItem({"parent":item})
         itemResult.setValue(self.config[PREPEND_VALUE] + item.getValue())
 
+        logging.debug("Prepending "+self.config[PREPEND_VALUE])
 
         return [itemResult]
