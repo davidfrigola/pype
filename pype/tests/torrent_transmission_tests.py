0f37981907bd5d4b7da113ca34f3014b6205e6f6
david <david.frigola@gmail.com>
2013-11-25 21:26:49 +0100

    #2 fake test

---
+++ pype/tests/torrent_transmission_tests.py
@@ -0,0 +1,16 @@
+
+import pype.torrent
+from mock import MagicMock
+
+""" Tests for transmission torrent processors"""
+
+
+
+
+def add_all_test():
+    # TODO
+    pass
+
+def add_all_failfirst_test():
+    # TODO
+    pass
