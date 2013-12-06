aaa2d9b2dc212332ff53a0177f46a8fefa0af958
dfguerro <dfguerro@OE1631.uk.ioko365.com>
2013-12-06 09:44:45 +0000

    #12 ScriptProcessor skeleton

--- pype/extra_processor.py
+++ pype/extra_processor.py
@@ -1,6 +1,8 @@
 from extra import *
 from extra_conditions import *
+from logging import *
 
+logger = logging.getLogger("pype.extra.processor")
 """
 DRY Processor : do not repeat yourself processor
 
@@ -8,12 +10,13 @@ Uses conditional processor, and a NOT_ALL condition with AlreadyProcessedConditi
 a Mongo datasource
 
 """
-DRY_METADATA_FLAG = "dry_processed"
+METADATA_DRY_FLAG = "dry_processed"
 class DRYProcessor(AbstractListProcessor):
 
     __config={}
     __alreadyExistsCondition = None
     __datasource = None
+
     def __init__(self,config):
 
         self.__config = config
@@ -33,5 +36,61 @@ class DRYProcessor(AbstractListProcessor):
            self.__datasource.store(item)
            logger.debug("Stored item - DRY processor "+str(item))
            #Set stored flag
-           item.setMetadataValue(DRY_METADATA_FLAG,True)
+           item.setMetadataValue(METADATA_DRY_FLAG,True)
            return [item]
+
+
+""" Script processor
+ Executes a script
+  * Per item
+  * Only once
+
+"""
+SCRIPT_EXEC_MODE = "executescriptmode"
+SCRIPT_EXEC_ONCE = "executescriptonce"
+SCRIPT_EXEC_PERITEM = "executescriptperitem"
+SCRIPT_EXEC_COMMAND = "executescritpcommand"
+#SCRIPT_EXEC_COMMAND_PARAM_ITEM_VALUE = "executescriptcommandparamitemvalue"
+class ScriptProcessor(AbstractProcessor):
+
+
+    __config = {}
+    __mode = None
+
+    def __init__(self,config):
+        self.__config = config
+        ##TODO Validate config
+        if not SCRIPT_EXEC_MODE in config:
+            raise "ScriptProcessor requires execution mode set in config"
+
+
+    def process(self,item):
+        if self.__mode==SCRIPT_EXEC_PERITEM:
+
+            self.__executescript(item)
+
+        else:
+            return [item]
+
+    def processList(self,items):
+        """ Process a list of items """
+        if self.__mode == SCRIPT_EXEC_ONCE:
+            self.__executescript(None)
+            pass
+        else:
+            return super(ScriptProcessor,self).processList(items)
+
+
+
+
+    def __executescript(self,item):
+        ## TODO
+        #see http://www.cyberciti.biz/faq/python-execute-unix-linux-command-examples/
+        #see http://stackoverflow.com/questions/14894993/running-windows-shell-commands-with-python
+        # os or subprocess??s
+
+        logger.info("Executing script " + str(self.__config[SCRIPT_EXEC_COMMAND]) + "for item " + str(item))
+
+        # execute script with item value as parameter
+        # just ignore the last parameter in the script
+        pass
