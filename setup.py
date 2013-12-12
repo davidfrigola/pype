e1f2593dbd9bd3045bc970fa9a2a42d702f05832
davidfrigola <david.frigola@gmail.com>
2013-12-12 15:20:30 +0000

    #16 First setup.py version, tested with 'python setup.py install' command

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pype',
+    version='0.1',
+    description='PYPE',
+    author='davidfrigola',
+    author_email='daviz@mailinator.com',
+    url='http://github.com/davidfrigola/pype',
+    packages=['pype'])
