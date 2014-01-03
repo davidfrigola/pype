c26507f7d0af2e19d4a747388d77c2960a9fb66f
davidfrigola <david.frigola@gmail.com>
2014-01-03 08:14:26 +0000

    #17 AddItemsProcessor first implementation and test

--- pype/extra_processor.py
+++ pype/extra_processor.py
@@ -100,3 +100,42 @@ class ScriptProcessor(AbstractProcessor):
         # execute script with item value as parameter
         # just ignore the last parameter in the script
         pass
+
+ADDITEMS_PREPEND = "additems_prepend"
+ADDITEMS_POSTPEND = "additems_postpend"
+
+class AddItemsProcessor(AbstractListProcessor):
+    """
+        Add fixed items to the stream.
+
+    """
+    __config = {}
+
+    __itemstoprepend = None
+    __itemstopostpend = None
+
+    def __init__(self,config):
+        self.__config = config
+        if ADDITEMS_PREPEND in config:
+            self.__itemstoprepend = config[ADDITEMS_PREPEND]
+        if ADDITEMS_POSTPEND in config:
+            self.__itemstopostpend = config[ADDITEMS_POSTPEND]
+
+
+    def process(self,item):
+        return self.__addItems([item])
+
+    def processList(self,items):
+        return self.__addItems(items)
+
+
+    def __addItems(self,items):
+        result = items
+        if self.__itemstoprepend is not None:
+            logger.debug("Prepending items")
+            result = self.__itemstoprepend + result
+
+        if self.__itemstopostpend is not None:
+            logger.debug("Postpending items")
+            result = result.extend(self.__itemstopostpend)
+        return result
---
+++ tests/test_extra_processor.py
@@ -0,0 +1,32 @@
+from pype.extra_processor import *
+from pype.model import *
+
+item1 = BaseItem(None)
+item1.setValue("item1")
+item2 = BaseItem(None)
+item2.setValue("item2")
+
+items = [item1,item2]
+
+def prepend_items_test():
+    addItem1 = BaseItem(None)
+    addItem1.setValue("addItem1")
+    addItem2 = BaseItem(None)
+    addItem2.setValue("addItem2")
+    prependItems = [addItem1,addItem2]
+    prependProcessor = AddItemsProcessor({ADDITEMS_PREPEND:prependItems})
+    result = prependProcessor.processList(items)
+
+    assert len(result)==len(items) + len(prependItems)
+
+
+def postpend_items_test():
+    addItem1 = BaseItem(None)
+    addItem1.setValue("addItem1")
+    addItem2 = BaseItem(None)
+    addItem2.setValue("addItem2")
+    postpendItems = [addItem1,addItem2]
+    postpendProcessor = AddItemsProcessor({ADDITEMS_POSTPEND:postpendItems})
+    result = postpendProcessor.processList(items)
+
+    assert len(result)==len(items) + len(prependItems)
