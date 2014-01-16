503b50908db57259e04a4261b2c0ea3285d57c44
david <david.frigola@gmail.com>
2014-01-16 21:48:11 +0100

    #20 Adding user agento config for htmlprocessor to sample

--- samples/htmlprocessor_sample.py
+++ samples/htmlprocessor_sample.py
@@ -7,7 +7,7 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 item = BaseItem(None,"https://github.com")
 
-htmlprocessor = HtmlProcessor({})
+htmlprocessor = HtmlProcessor({HTML_REQUEST_AGENT_HEADER:"Some user-agent"})
 
 result = htmlprocessor.process(item)
 
