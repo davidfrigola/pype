2ea7c7c89d78825d297e00eff2552ae73f25abe9
david.frigola <david.frigola@gmail.com>
2014-02-01 20:10:00 +0100

    [no-issue] Renaming _self to self

--- pype/rss.py
+++ pype/rss.py
@@ -12,14 +12,14 @@ class RssProcessor(AbstractListProcessor):
 	#default field to retrieve from rss items : link
 	field = "link"
 
-	def __init__(_self,config):
+	def __init__(self,config):
 		""" Configuration
 		 	Dict with field key, should be 'link' or 'title'
 		 """
-		_self.field = config["field"]
-		logger.debug("Set field as %s",_self.field)
+		self.field = config["field"]
+		logger.debug("Set field as %s",self.field)
 
-	def process(_self,item):
+	def process(self,item):
 		""" Process a RSS item , retrieving the configured field as a string
 
 			- item should contain a valid RSS url
@@ -33,22 +33,22 @@ class RssProcessor(AbstractListProcessor):
 
 		for entry in feedresults.entries:
 			resultEntry = BaseItem({"parent":item})
-			if _self.field == "link":
+			if self.field == "link":
 				resultEntry.setValue(entry.link)
-			elif _self.field == "title":
+			elif self.field == "title":
 				resultEntry.setValue(entry.title)
-			elif _self.field == "content":
+			elif self.field == "content":
 				resultEntry.setValue(entry.content)
 			else:
 
 				""" Unknown field to obtain. Nothing to return """
-				logger.warning("Unknown %s field",str(_self.field))
+				logger.warning("Unknown %s field",str(self.field))
 				return None
 
 			if resultEntry is not None:
 				#Add always title as metadata
 				resultEntry.setMetadataValue("title",entry.title)
-				logger.debug("Entry value for '" + _self.field + "'= " + str(resultEntry.getValue()))
+				logger.debug("Entry value for '" + self.field + "'= " + str(resultEntry.getValue()))
 				result.append(resultEntry)
 		logger.debug("Returning %s elements",str(len(result)))
 		return result
